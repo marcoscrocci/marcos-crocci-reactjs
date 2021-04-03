70cf457cd73caf3b9e55c8e26ece917255e1816d
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-04-02 22:04:05 -0300

    Users - Auth

--- src/pages/TestFirebase/index.jsx
+++ src/pages/TestFirebase/index.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 //import GlobalContext from '../../context/GlobalContext'
 import firebase from 'firebase';
 import FirebaseClient from '../../FirebaseClient'
 
-export default function TestFirebase (props) {
+export default function TestFirebase(props) {
     FirebaseClient();
+    //const { state, dispatch } = useContext(GlobalContext);
+    const history = useHistory();
 
     const [Pontuacao, setPotuacao] = useState(null);
+    const [UsuarioAutenticado, setUsuarioAutenticado] = useState(null);
+    const [Funcionarios, setFuncionarios] = useState(null);
 
     const salvarDados = () => {
         //alert("Salvar Dados");
@@ -58,9 +62,23 @@ export default function TestFirebase (props) {
         )
     }
 
-     
+    const cadastrarUsuario = () => {
+        var email = "curso.firebase@gmail.com";
+        var senha = "secret12345";
+        const usuario = firebase.auth();
+        usuario.createUserWithEmailAndPassword(email, senha)
+            .then(() => {
+                alert("Usuário criado com sucesso!");
+            })
+            .catch((error) => {
+                alert(`Código: ${error.code} - Mensagem: ${error.message}`);
+            });
+    }
+
+
     const listarDados = () => {
         var pontuacao = firebase.database().ref("pontuacao");
+        var funcionarios = firebase.database().ref("funcionarios");
         // Olha como o Firebase é incrível, podemos...
         // Criar um método de escuta, para quando os dados do Firebase for alterados
         // Não precisa fazer uma nova requisição no banco, o banco que avisa o cliente, olha eu fui 
@@ -68,15 +86,53 @@ export default function TestFirebase (props) {
         pontuacao.on('value', (snapshot) => {
             setPotuacao(snapshot.val());
         })
+        funcionarios.on('value', (snapshot) => {
+            setFuncionarios(snapshot.val());
+        })
+    }
+
+    const verificarUsuarioAutenticado = () => {
+        const usuario = firebase.auth();
+        const usuarioAtual = usuario.currentUser;
+        if (usuarioAtual) {
+            alert(JSON.stringify(usuarioAtual));
+        } else {
+            alert("Não há usuários autenticados no Firebase!");
+        }
+    }
+
+    useEffect(() => {
+        const usuario = firebase.auth();
+        usuario.onAuthStateChanged((usuarioAtual) => {
+            setUsuarioAutenticado(usuarioAtual);
+        })
+    }, [])
+
+    const usuarioSair = () => {
+        const usuario = firebase.auth();
+        usuario.signOut();
+    }
+
+    const autenticarUsuario = () => {
+        var email = "curso.firebase@gmail.com";
+        var senha = "secret12345";
+        const usuario = firebase.auth();
+        usuario.signInWithEmailAndPassword(email, senha)
+            .then(() => {
+                alert("Usuário autenticado com sucesso!");
+            })
+            .catch((error) => {
+                alert(`Código: ${error.code} - Mensagem: ${error.message}`);
+            });
     }
 
 
-    //const { state, dispatch } = useContext(GlobalContext);
-    const history = useHistory();
     return (
-        <div>
+        <div style={{ margin: 20 }}>
             <p>Teste do Firebase</p>
             <p>Pontuação: {Pontuacao}</p>
+            
+            <p>Usuário Autenticado: {UsuarioAutenticado && UsuarioAutenticado.email}</p>
             <button onClick={() => salvarDados()}>Salvar Dados</button>
             <br />
             <button onClick={() => removerDados()}>Remover Dados</button>
@@ -91,7 +147,23 @@ export default function TestFirebase (props) {
             <br />
             <button onClick={() => listarDados()}>Listar Dados</button>
             <br />
+            <button onClick={() => cadastrarUsuario()}>Criar Usuário</button>
+            <br />
+            <button onClick={() => verificarUsuarioAutenticado()}>Verificar Usuário Autenticado</button>
+            <br />
+            <button onClick={() => usuarioSair()}>Usuário - Sair</button>
+            <br />
+            <button onClick={() => autenticarUsuario()}>Usuário - Autenticar</button>
+            <br />
             <button onClick={() => history.push('/')}>Principal</button>
+            <br />
+            <br />
+            <label>Funcionários:</label><br />
+            <textarea 
+                style={{ width: '70%', height: '200px' }} 
+                readOnly={true}
+                value={JSON.stringify(Funcionarios, null, 4)} 
+            />
         </div>
- )
+    )
 }
